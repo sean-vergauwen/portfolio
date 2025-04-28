39e0652a6325beeb78d254075aaa36ce237d4c4b
sean-vergauwen <92988883+sean-vergauwen@users.noreply.github.com>
2025-04-28 14:31:39 +0200

    FIX leetcode link

--- app/config.ts
+++ app/config.ts
@@ -10,7 +10,7 @@ export const metaData = {
 export const socialLinks = {
   twitter: "https://x.com/sean_vergauwen",
   github: "https://github.com/sean-vergauwen",
-  leetcode: "https://leetcode.com/u/sean_vergauwen",
+  leetcode: "https://leetcode.com/u/sean-vergauwen",
   instagram: "https://www.instagram.com/sean.vergauwen",
   linkedin: "https://www.linkedin.com/in/sean-vergauwen",
   email: "mailto:seanvr93@gmail.com",
